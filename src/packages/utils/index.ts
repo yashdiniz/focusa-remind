4c5ac9fe46cf8b31eec5deb6da9dba7b7ea20132
yashdiniz <26359876+yashdiniz@users.noreply.github.com>
2025-09-25 23:26:23 +0530

    feat: add humanTime

--- src/packages/utils/index.ts
+++ src/packages/utils/index.ts
@@ -31,6 +31,42 @@ export function validateTimezone(tz: string) {
 }
 
 /**
+ * Print a human readable timestamp to the terminal given a number representing seconds.
+ * EDIT: modified for typescript
+ *
+ * Original Author: Dave Eddy <dave@daveeddy.com>
+ * Date: 8/18/2014
+ * License: MIT
+ */
+export function humanTime(seconds: Date | number) {
+    if (seconds instanceof Date) seconds = Math.round((Date.now() - seconds.getTime()) / 1000);
+    const suffix = seconds < 0 ? 'from now' : 'ago';
+    seconds = Math.abs(seconds);
+
+    const times = [
+        seconds / 60 / 60 / 24 / 365, // years
+        seconds / 60 / 60 / 24 / 30,  // months
+        seconds / 60 / 60 / 24 / 7,   // weeks
+        seconds / 60 / 60 / 24,       // days
+        seconds / 60 / 60,            // hours
+        seconds / 60,                 // minutes
+        seconds                       // seconds
+    ];
+    const names = ['year', 'month', 'week', 'day', 'hour', 'minute', 'second'];
+
+    for (let i = 0; i < names.length; i++) {
+        const time = Math.floor(times[i]!);
+        let name = names[i]!;
+        if (time > 1)
+            name += 's';
+
+        if (time >= 1)
+            return `${time} ${name} ${suffix}`;
+    }
+    return '0 seconds ' + suffix;
+}
+
+/**
  * Fetches a user from the database based on platform and chatId.
  * @param platform currently only 'telegram' is supported
  * @param chatId chatId from the platform
