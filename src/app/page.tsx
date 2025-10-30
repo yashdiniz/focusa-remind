db7de248003cfa7a1669fc030e2cf88649eb48b5
yashdiniz <26359876+yashdiniz@users.noreply.github.com>
2025-10-30 17:53:55 +0530

    feat: add telegram mini app for reminders

---
+++ src/app/page.tsx
@@ -0,0 +1,30 @@
+'use client'
+// Import the necessary styles globally
+import '@telegram-apps/telegram-ui/dist/styles.css';
+
+// Import components from the library
+import { AppRoot, Cell, List, Section } from '@telegram-apps/telegram-ui';
+import { retrieveRawInitData } from '@tma.js/sdk-react';
+
+// Example data for rendering list cells
+const cellsTexts = ['Chat Settings', 'Data and Storage', 'Devices'];
+
+export default function App() {
+    const initDataRaw = retrieveRawInitData()
+    return (
+        <AppRoot>
+            {/* List component to display a collection of items */}
+            <List>
+                {/* Section component to group items within the list */}
+                <Section header="Header for the section" footer={initDataRaw}>
+                    {/* Mapping through the cells data to render Cell components */}
+                    {cellsTexts.map((cellText, index) => (
+                        <Cell key={index}>
+                            {cellText}
+                        </Cell>
+                    ))}
+                </Section>
+            </List>
+        </AppRoot>
+    )
+};
