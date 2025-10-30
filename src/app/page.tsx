911b1452e4b47f2dfe5161e5cce59c5f863064ff
yashdiniz <26359876+yashdiniz@users.noreply.github.com>
2025-10-30 18:04:40 +0530

    fix: pre-render issue from next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,23 @@ import '@telegram-apps/telegram-ui/dist/styles.css';
 // Import components from the library
 import { AppRoot, Cell, List, Section } from '@telegram-apps/telegram-ui';
 import { retrieveRawInitData } from '@tma.js/sdk-react';
+import { useEffect, useState } from 'react';
 
 // Example data for rendering list cells
 const cellsTexts = ['Chat Settings', 'Data and Storage', 'Devices'];
 
 export default function App() {
-    const initDataRaw = retrieveRawInitData()
+    const [initData, setInitData] = useState<string>('');
+    useEffect(() => {
+        setInitData(retrieveRawInitData() ?? '');
+    }, []);
+
     return (
         <AppRoot>
             {/* List component to display a collection of items */}
             <List>
                 {/* Section component to group items within the list */}
-                <Section header="Header for the section" footer={initDataRaw}>
+                <Section header="Header for the section" footer={initData}>
                     {/* Mapping through the cells data to render Cell components */}
                     {cellsTexts.map((cellText, index) => (
                         <Cell key={index}>
